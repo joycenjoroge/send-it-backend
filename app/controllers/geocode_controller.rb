9d638c56564a11a559275dcc0c2c8180f8689b0e
jesse <jesse.kibaara@student.moringaschool.com>
2023-07-05 23:17:46 +0300

    apikey

--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -1,7 +1,7 @@
 class GeocodeController < ApplicationController
   def proxy
     address = params[:address]
-    api_key = 'YOUR_API_KEY' # Replace with your actual geocoding API key
+    api_key = 'AIzaSyArxLH3o4xPe93ks1-SXsRlYm4YcCerSbk' # Replace with your actual geocoding API key
 
     url = URI.parse("https://geocoding-api.com/?address=#{address}&apiKey=#{api_key}")
     req = Net::HTTP::Get.new(url.to_s)
